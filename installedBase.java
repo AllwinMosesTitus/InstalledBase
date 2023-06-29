cc30239e1a701c77b18bd5b5f0f63c840c6ed52c
Allwin <allwintitus@gmail.com>
2023-06-29 14:02:30 +0000

    7th commit from Feature_1

--- installedBase.java
+++ installedBase.java
@@ -2,7 +2,8 @@ class InstalledBase {
     public static void main(String[] args) {
         System.out.println("Hello, Allwin Moses Titus!, Welcome, new edit, second, third edit by allwin user");
 
-       system.out.println("5th commit in Feature_1 branch");
-       system.out.println("6th commit from feature_1 branch");       
+       System.out.println("5th commit in Feature_1 branch");
+       System.out.println("6th commit from feature_1 branch");  
+       System.out.println("7th commit from Feature_1 branch");     
     }
 
