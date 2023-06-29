9b98476f227945a76518b2d522fdd085c8bcb707
AllwinMosesTitus <137503701+AllwinMosesTitus@users.noreply.github.com>
2023-06-29 20:50:58 +0530

    Master commit - overall 8th commit

--- installedBase.java
+++ installedBase.java
@@ -3,6 +3,7 @@ class InstalledBase {
         System.out.println("Hello, Allwin Moses Titus!, Welcome, new edit, second, third edit by allwin user");
 
        system.out.println("5th commit in Feature_1 branch");
-       system.out.println("6th commit from feature_1 branch");       
+       system.out.println("6th commit from feature_1 branch");
+      /* Edited inside of Remote Repo*/
     }
 
