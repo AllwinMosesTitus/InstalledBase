c40dc654a2c56eb3fce105cfe6064df896e769c7
Allwin <allwintitus@gmail.com>
2023-06-29 08:24:20 +0000

    3rd commit

--- installedBase.java
+++ installedBase.java
@@ -1,5 +1,5 @@
 class InstalledBase {
     public static void main(String[] args) {
-        System.out.println("Hello, Allwin Moses Titus!, Welcome, new edit"); 
+        System.out.println("Hello, Allwin Moses Titus!, Welcome, new edit, second edite"); 
     }
 
